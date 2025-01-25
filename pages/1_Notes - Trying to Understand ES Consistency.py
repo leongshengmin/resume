0d91dce5f755320c76c73a90458d43f1341a697d
shengmin.leong <leongshengmin@leongs-air.lan>
2025-01-25 09:05:40 +0800

    Update header.

--- pages/1_Notes - Trying to Understand ES Consistency.py
+++ pages/1_Notes - Trying to Understand ES Consistency.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Notes to self - Trying to Understand How ES Does Consistency",
+    page_title="Trying to Understand How ES Does Consistency",
     page_icon="",
 )
 
 st.markdown(
     """
-            # Notes to self - Trying to Understand How ES Does Consistency
+            # Trying to Understand How Elasticsearch Does Consistency
             *Document is still a WIP*
 """
 )
@@ -15,4 +15,4 @@ with open("pages/es_consistency_principles.md", "r") as f:
     data = f.read()
 
 st.markdown(data)
-st.sidebar.header("Notes to self - Trying to Understand How ES Does Consistency")
+st.sidebar.header("Trying to Understand How Elasticsearch Does Consistency")
